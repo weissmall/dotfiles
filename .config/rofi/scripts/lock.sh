511d18e61ae6be8c75758646c899b58193574b90
weissmall <dany.weissmall@gmail.com>
2025-10-24 16:06:57 +0300

    feat: dynamic niri & waypaper lockscreen
    - Updated colors to more pastel palette
    - Added functions to determine current niri outputs
    - Based on each output script will try to determine current background
      image from waypaper
    - Should currently work with any outputs

--- .config/rofi/scripts/lock.sh
+++ .config/rofi/scripts/lock.sh
@@ -5,26 +5,73 @@ function getWallpaperByOutput() {
   echo $outputsJson
 }
 
+function getWallpaperByOutputOrAll() {
+  output=$1
+  wallpaper=$(getWallpaperByOutput "${output}")
+  if [[ -n $wallpaper ]]; then
+    echo $wallpaper
+  else
+    echo $(getWallpaperByOutput "All")
+  fi
+}
+
+function getImagesPerOutput() {
+  niriOutputs=$(niri msg outputs | grep "Output" | grep -oE '\(.*\)' | tr -d '()')
+  niriOutputsArray=($niriOutputs)
+
+  result=""
+  for i in "${niriOutputsArray[@]}"; do
+    wallpaper=$(getWallpaperByOutputOrAll $i)
+    result+="--image $i:$wallpaper "
+  done
+
+  result=${result% }
+  echo $result
+}
+
+
+
 function lock() {
   if [[ -x '/usr/bin/swaylock' ]]; then
     swaylock \
       --daemonize\
-      --color "#282a2b"\
-      --inside-color "#282a2b"\
-      --inside-clear-color "#eeeeee"\
-      --ring-color "#3b758c"\
-      --ring-clear-color "#9fca56"\
-      --ring-ver-color "#3498db"\
+\
+      --inside-clear-color "#3E4244"\
+      --line-clear-color "#3E4244"\
+      --ring-clear-color "#A4C9E3"\
+      --text-clear-color "#FFFFFF"\
+\
+      --inside-ver-color "#35424A"\
+      --line-ver-color "#35424A"\
+      --ring-ver-color "#A4C9E3"\
+      --text-ver-color "#FFFFFF"\
+\
+      --inside-wrong-color "#4A3535"\
+      --line-wrong-color "#4A3535"\
+      --ring-wrong-color "#E3A4A4"\
+      --text-wrong-color "#FFFFFF"\
+\
+      --inside-color "#3E4244"\
+      --line-color "#3E4244"\
+      --ring-color "#A4C9E3"\
+      --text-color "#FFFFFF"\
+\
+      --text-caps-lock-color "#FFFFFF"\
+\
+      --key-hl-color "#FFFFFF"\
+      --bs-hl-color "#808080"\
+\
       --show-failed-attempts\
       --fade-in 0.2\
       --grace 1\
       --effect-blur 2x1\
       --effect-vignette 0.5:0.5\
       --ignore-empty-password\
-      --image eDP-1:$(getWallpaperByOutput "eDP-1")\
-      --image HDMI-A-1:$(getWallpaperByOutput "HDMI-A-1")\
-      --clock
+      $(getImagesPerOutput) \
+      --clock \
+      -e
   fi
 }
+
 
 lock >> $HOME/lock.log

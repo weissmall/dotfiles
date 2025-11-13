8c3c0d3bf17bebf976b120a238710a946a753b1e
weissmall <dany.weissmall@gmail.com>
2025-11-14 00:50:50 +0300

    feat!: deleted ags for submodule future

--- .config/ags/widgets/Bar/Workspaces/Niri.tsx
+++
@@ -1,8 +0,0 @@
-
-function getAllWorkspaces() {
-    const command = 'niri msg workspaces | grep -E "[* ][0-9]" | wc -l'
-}
-
-function getActiveWorkspace() {
-    const command = 'niri msg workspaces | grep -E "\* [0-9]" | cut -d \' \' -f3'
-}
--- .config/ags/widgets/material_icon.tsx
+++
@@ -1,7 +0,0 @@
-import { Widget } from "astal/gtk3"
-
-export const MaterialIcon = (icon: string, size: number, props = {}) => new Widget.Label({
-    className: `icon-material txt-${size}`,
-    label: icon,
-    ...props,
-})
